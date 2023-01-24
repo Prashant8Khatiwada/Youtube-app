95d46e7d5d9225634f83ce9b615ac563088bc171
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2023-01-24 22:38:42 +0545

    ..

---
+++ src/components/Videos.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./";
+import Spinner from "./Spinner";
+function Videos({ videos, direction }) {
+  if (!videos?.length) return <Spinner />;
+  return (
+    <Stack
+      className="air"
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      sx={{ gap: { md: 2, xs: 1 } }}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+}
+
+export default Videos;
